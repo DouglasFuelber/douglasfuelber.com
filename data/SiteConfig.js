61585f0bc13ae6cbd36a046e7079d4660abc6bae
Douglas Fuelber <dffuelber@gmail.com>
2018-09-17 13:10:45 -0300

    Project configuration

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  siteTitle: "Douglas Fuelber - Developer", // Site title.
+  siteTitle: "Douglas Fuelber", // Site title.
   siteTitleShort: "Douglas Fuelber", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "Douglas Fuelber - Developer", // Alternative site title for SEO.
-  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
+  siteLogo: "", //"/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://www.douglasfuelber.com", // Domain of your website without pathPrefix.
   pathPrefix: "/site", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
@@ -11,7 +11,7 @@ module.exports = {
   siteFBAppID: "", // FB Application ID for using app insights
   siteGATrackingID: "", // Tracking code ID for google analytics.
   disqusShortname: "", // Disqus shortname.
-  postDefaultCategoryID: "Dev", // Default category for posts.
+  postDefaultCategoryID: "Programação", // Default category for posts.
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "DD/MM/YYYY", // Date format for display.
   userName: "Douglas Fuelber", // Username to display in the author segment.
@@ -19,7 +19,7 @@ module.exports = {
   userLocation: "Brasil", // User location to display in the author segment.
   userAvatar: "https://scontent.fpoa11-1.fna.fbcdn.net/v/t1.0-9/30713004_1522740204502520_2647134068470185984_n.jpg?_nc_cat=0&oh=ce24a65e4743d07697af4121f44497eb&oe=5C26A9CE", // User avatar to display in the author segment.
   userDescription:
-    "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+  "Como programador, gosto de aprender novas tecnologias e linguagens, buscando não me limitar em apenas aprimorar o conhecimento que já possuo. Além disso, gosto muito de séries, filmes, esportes e de conhecer novos lugares.", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
