9ab88d8b0b822b722a4eafdf583c465e422e99dd
DouglasFuelber <dffuelber@gmail.com>
2019-01-19 17:28:34 -0200

    Update info 2

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteTitle: "Douglas Fuelber", // Site title.
   siteTitleShort: "Douglas Dev", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "Douglas Fuelber - Programmer", // Alternative site title for SEO.
+  siteTitleAlt: "Douglas Fuelber - Full Stack Developer", // Alternative site title for SEO.
   siteLogo: "/logos/favicon.png", // Logo used for SEO and manifest.
   siteUrl: "https://www.douglasfuelber.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
-  siteDescription: "Douglas Fuelber - Bachelor in Computer Science at Unisc and programmer at Conectt.", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "Douglas Fuelber - Bachelor in Computer Science at Unisc and full stack developer at Conectt.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "", // FB Application ID for using app insights
   siteGATrackingID: "UA-99213358-1", // Tracking code ID for google analytics.
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,7 +20,7 @@ class Index extends React.Component {
           <div id="main_banner">
             <div id="page_title">
               <h1>Douglas Fuelber</h1>
-              <h2>Programmer</h2>
+              <h2>Full Stack Developer</h2>
             </div>
           </div>
 
