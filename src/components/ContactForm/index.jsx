777eef5dac1e4da3cde8477edea5fa3dfb205702
DouglasFuelber <dffuelber@gmail.com>
2019-01-20 18:43:26 -0200

    Finish site contact

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Button from "react-md/lib/Buttons";
 import { TextField } from 'react-md';
 import "./ContactForm.scss";
 
@@ -7,36 +8,43 @@ class ContactForm extends Component {
         return (
             <div className="contact-container mobile-fix">
                 <div className="contact-wrapper md-cell--center">
-                    <div className="md-grid md-cell--12">
+                    <div className="md-cell--12">
                         <h3>Contact</h3>
-                        <div id="form">                        
-                            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                                <input type="hidden" name="form-name" value="contact" />
-                                <div className="md-grid">
-                                    <TextField
-                                        id="name"
-                                        label="Name"
-                                        className="md-cell--12 md-cell--bottom"
-                                        />
-                                    <TextField
-                                        id="email"
-                                        label="Email"
-                                        className="md-cell--12 md-cell--bottom"
-                                        type="email"
-                                        />
-                                    <TextField
-                                        id="message"
-                                        label="Message"
-                                        rows={5}
-                                        className="md-cell--12 md-cell--bottom"
-                                        />
-                                    </div>
-                                <div data-netlify-recaptcha="true"></div>
-                                <p>
-                                    <button type="submit">Send</button>
-                                </p>
-                            </form>
-                        </div>
+                        <p>If you want to contact me, leave me a message:</p>
+                    </div>
+                    <div id="form">                        
+                        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                            <input type="hidden" name="form-name" value="contact" />
+                            <div className="md-grid">
+                                <TextField
+                                    id="name"
+                                    label="Name"
+                                    className="md-cell--12 md-cell--bottom"
+                                    required
+                                    />
+                                <TextField
+                                    id="email"
+                                    label="Email"
+                                    className="md-cell--12 md-cell--bottom"
+                                    type="email"
+                                    required
+                                    />
+                                <TextField
+                                    id="message"
+                                    label="Message"
+                                    rows={5}
+                                    className="md-cell--12 md-cell--bottom"
+                                    required
+                                    />
+                                <div className="md-cell--12">
+                                    <div data-netlify-recaptcha="true"></div>
+                                </div>
+                                <div className="md-cell--12">
+                                    <br/>
+                                    <Button flat className="submit-button" type="submit">Send</Button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
