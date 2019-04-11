23af4ecc151da834a442378522ed2a44563e78fe
Douglas Fuelber <douglas.fuelber@conectt.com.br>
2019-04-11 13:10:21 -0300

    Form validation on submit

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -6,72 +6,88 @@ import { TextField } from 'react-md';
 import "./ContactForm.scss";
 
 class ContactForm extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
- 
+
         this.state = {
             fields: {},
             errors: {}
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
-     } 
-     handleValidation(){
-         let fields = this.state.fields;
-         let errors = {};
-         let formIsValid = true;
- 
-         //Name
-         if(!fields["Name"]){
+    }
+    handleValidation() {
+        let fields = this.state.fields;
+        let errors = {};
+        let formIsValid = true;
+
+        //Name
+        if (!fields["Name"]) {
             formIsValid = false;
-            errors["Name"] = "Cannot be empty";
-         }
- 
-         if(typeof fields["Name"] !== "undefined"){
-            if(!fields["Name"].match(/^[a-zA-Z]+$/)){
-               formIsValid = false;
-               errors["Name"] = "Only letters";
-            }        
-         }
- 
-         //Email
-         if(!fields["Email"]){
+            errors["Name"] = "This field is required";
+        }
+        else if (typeof fields["Name"] !== "undefined") {
+            if (!fields["Name"].match(/^[a-zA-Z]+$/)) {
+                formIsValid = false;
+                errors["Name"] = "Only letters are allowed";
+            }
+            else {
+                errors["Name"] = "";
+            }
+        }
+        else {
+            errors["Name"] = "";
+        }
+
+        //Email
+        if (!fields["Email"]) {
             formIsValid = false;
-            errors["Email"] = "Cannot be empty";
-         }
- 
-         if(typeof fields["Email"] !== "undefined"){
+            errors["Email"] = "This field is required";
+        }
+        else if (typeof fields["Email"] !== "undefined") {
             let lastAtPos = fields["Email"].lastIndexOf('@');
             let lastDotPos = fields["Email"].lastIndexOf('.');
- 
+
             if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["Email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["Email"].length - lastDotPos) > 2)) {
-               formIsValid = false;
-               errors["Email"] = "Email is not valid";
-             }
-        }  
- 
-        this.setState({errors: errors});
+                formIsValid = false;
+                errors["Email"] = "Email is not valid";
+            }
+        } else {
+            errors["Email"] = "";
+        }
+
+        //Message
+        if (!fields["Message"]) {
+            formIsValid = false;
+            errors["Message"] = "This field is required";
+        } else {
+            errors["Message"] = "";
+        }
+
+        //Recaptcha
+        if (!fields["Recaptcha"]) {
+            formIsValid = false;
+            errors["Recaptcha"] = "This field is required";
+        } else {
+            errors["Recaptcha"] = "";
+        }        
+
+        this.setState({ errors: errors });
         return formIsValid;
     }
- 
-    handleSubmit(e){
-         e.preventDefault();
- 
-         if(this.handleValidation()){
-            alert("Form submitted");
-         }else{
-            alert("Form has errors.")
-         }
 
-        /*const recaptchaValue = recaptchaRef.current.getValue();
-        this.props.onSubmit(recaptchaValue);*/
- 
-     } 
-     handleChange(field, e){         
-         let fields = this.state.fields;
-         fields[field] = e.target.value;        
-         this.setState({fields});
-     }
+    handleSubmit(e) {
+        e.preventDefault();
+
+        if (this.handleValidation()) {
+            e.target.submit();
+        }
+    }
+    handleChange(field, e) {
+        let fields = this.state.fields;
+        fields[field] = e;
+        this.setState({ fields });
+    }
     render() {
         const recaptchaRef = React.createRef();
         return (
@@ -81,10 +97,11 @@ class ContactForm extends Component {
                         <h3>Contact</h3>
                         <p>If you want to contact me, leave me a message:</p>
                     </div>
-                    <div id="form">                        
+                    <div id="form">
                         <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.handleSubmit}>
                             <input type="hidden" name="form-name" value="contact" />
                             <div className="md-grid">
+
                                 <TextField
                                     id="name"
                                     label="Name"
@@ -93,9 +110,9 @@ class ContactForm extends Component {
                                     className="md-cell--12 md-cell--bottom"
                                     onChange={this.handleChange.bind(this, "Name")}
                                     value={this.state.fields["Name"]}
-                                    required
-                                    />
-                                    <span style={{color: "red"}}>{this.state.errors["Name"]}</span>
+                                    required />
+                                <div class="md-text-field-message-container md-full-width md-text--error">{this.state.errors["Name"]}</div>
+
                                 <TextField
                                     id="email"
                                     label="Email"
@@ -105,25 +122,32 @@ class ContactForm extends Component {
                                     onChange={this.handleChange.bind(this, "Email")}
                                     value={this.state.fields["Email"]}
                                     ref="Email"
-                                    required
-                                    />
-                                    <span style={{color: "red"}}>{this.state.errors["Email"]}</span>
+                                    required />
+                                <div class="md-text-field-message-container md-full-width md-text--error">{this.state.errors["Email"]}</div>
+
                                 <TextField
                                     id="message"
                                     label="Message"
                                     name="Message"
                                     rows={5}
                                     className="md-cell--12 md-cell--bottom"
-                                    required
-                                    />
+                                    onChange={this.handleChange.bind(this, "Message")}
+                                    value={this.state.fields["Message"]}
+                                    ref="Message"
+                                    required />
+                                <div class="md-text-field-message-container md-full-width md-text--error">{this.state.errors["Message"]}</div>
+
                                 <div className="md-cell--6">
-                                    <br/>
+                                    <br />
                                     <ReCAPTCHA
                                         ref={recaptchaRef}
-                                        sitekey={config.recaptchaPublicKey} />
+                                        sitekey={config.recaptchaPublicKey}
+                                        onChange={this.handleChange.bind(this, "Recaptcha")} />
+                                    <div class="md-text-field-message-container md-full-width md-text--error">{this.state.errors["Recaptcha"]}</div>
+                                    
                                 </div>
                                 <div className="md-cell--6">
-                                    <br/>
+                                    <br />
                                     <Button flat className="submit-button" type="submit">Send</Button>
                                 </div>
                             </div>
