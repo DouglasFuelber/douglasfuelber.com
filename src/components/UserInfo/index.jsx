d2fd4d85eb2e205fcd006eced0b408336718d88f
Douglas Fuelber <dffuelber@gmail.com>
2019-09-19 17:52:46 -0300

    Removed Twitter unused dependencie

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -5,7 +5,6 @@ import CardText from "react-md/lib/Cards/CardText";
 import Avatar from "react-md/lib/Avatars";
 import FontIcon from "react-md/lib/FontIcons";
 import IconSeparator from "react-md/lib/Helpers/IconSeparator";
-import { Follow } from "react-twitter-widgets";
 import UserLinks from "../UserLinks";
 import "./UserInfo.scss";
 
@@ -39,16 +38,6 @@ class UserInfo extends Component {
           expander={!expanded}
           avatar={userAvatar && <Avatar src={userAvatar} role="presentation" />}
           title={userName && userName}
-          subtitle={
-            userTwitter ? (
-              <Follow
-                username={userTwitter}
-                options={{ count: expanded ? "none" : "none" }}
-              />
-            ) : (
-              "Autor"
-            )
-          }
         />
         <CardText expandable={!expanded}>
           {userLocation && (
