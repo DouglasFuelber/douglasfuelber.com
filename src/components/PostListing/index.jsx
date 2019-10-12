454565084084410507b1778ebe5ad49bffff847a
DouglasFuelber <dffuelber@gmail.com>
2019-10-11 22:05:44 -0300

    Added category list to post listing

--- src/components/PostListing/index.jsx
+++ src/components/PostListing/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PostCategoriesListing from "../PostCategoriesListing";
 import PostPreview from "../PostPreview";
 import "./PostListing.scss";
 
@@ -22,12 +23,15 @@ class PostListing extends React.Component {
   render() {
     const postList = this.getPostList();
     return (
-      <div className="md-grid md-grid--no-spacing md-cell--middle primary_bg">
-        <div id="post-container" className="md-grid md-cell--8 mobile-fix">
+      <div id="posts-wrapper" className="md-grid md-grid--no-spacing md-cell--middle primary_bg">
+        <div className="md-cell--3 md-cell--order-2-desktop mobile-fix">
+          <PostCategoriesListing />
+        </div>
+        <div id="post-container" className="md-cell--9 mobile-fix">
           {postList.map(post => (
             <PostPreview key={post.title} postInfo={post} />
           ))}
-        </div>
+        </div>        
       </div>
     );
   }
