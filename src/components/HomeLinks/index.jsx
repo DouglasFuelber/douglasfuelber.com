3fdda2717a872ec52ab63762977c21c9e3a8c184
DouglasFuelber <dffuelber@gmail.com>
2019-10-29 13:18:58 -0300

    Refactored HomeLinks class with arrow function

--- src/components/HomeLinks/index.jsx
+++ src/components/HomeLinks/index.jsx
@@ -1,33 +1,24 @@
 import { Home } from "../../../data/SiteContent";
-import FontIcon from "react-md/lib/FontIcons";
 import { Link } from "gatsby";
-import React, { Component } from "react";
+import React from "react";
 
 import "./HomeLinks.scss";
 
-class HomeLinks extends Component {
-  render() {
-    const { collumnSize } = this.props;
-    return (
-      <div className="links-container">
-        <div className="md-grid links-wrapper">
-          { Home.homeLinks.map(item => (
-            <div className={`link md-cell--center md-text-center md-cell--${collumnSize}`}>
-              <Link to={item.link}>
-                <div className="icon">
-                  <i className={`fas ${item.icon} fa-2x`}></i>
-                </div>
-                <div className="text">
-                  <h5>{item.secondaryTitle}</h5>
-                  <h3>{item.primaryTitle}</h3>
-                </div>              
-              </Link>
-            </div>  
-          )) }
+export default ({ collumnSize }) =>
+  <div className="links-container">
+    <div className="md-grid links-wrapper">
+      {Home.homeLinks.map(item => (
+        <div className={`link md-cell--center md-text-center md-cell--${collumnSize}`}>
+          <Link to={item.link}>
+            <div className="icon">
+              <i className={`fas ${item.icon} fa-2x`}></i>
+            </div>
+            <div className="text">
+              <h5>{item.secondaryTitle}</h5>
+              <h3>{item.primaryTitle}</h3>
+            </div>
+          </Link>
         </div>
-      </div>
-    );
-  }
-}
-
-export default HomeLinks;
+      ))}
+    </div>
+  </div>
