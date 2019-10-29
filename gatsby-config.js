81117b6d787f7dc9f84634bcfc397e09f2bb2630
DouglasFuelber <dffuelber@gmail.com>
2019-10-28 23:08:22 -0300

    Removed gatsby-plugin-intl plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -209,18 +209,5 @@ module.exports = {
         shortname: `douglasfuelber`
       }
     },
-    {
-      resolve: `gatsby-plugin-intl`,
-      options: {
-        // language JSON resource path
-        path: `${__dirname}/src/intl`,
-        // supported language
-        languages: [`en`, `pt`],
-        // language file path
-        defaultLanguage: `en`,
-        // option to redirect to `/en` when connecting `/`
-        redirect: true,
-      },
-    }
   ]
 };
