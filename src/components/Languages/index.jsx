35d167c664e99c3fc91870a72a2833f36b5159d7
DouglasFuelber <dffuelber@gmail.com>
2019-11-01 13:29:28 -0300

    Finished language selector

--- src/components/Languages/index.jsx
+++ src/components/Languages/index.jsx
@@ -3,19 +3,26 @@ import { SelectField } from 'react-md';
 import { useIntl, changeLocale } from "gatsby-plugin-intl";
 import { languages } from "../../../data/SiteConfig";
 
+import "./Languages.scss";
+
 export default () => {
     const intl = useIntl();
-    return <SelectField
-            id="select-field-default-value"
-            label="Language"
-            defaultValue={intl.locale}
-            onChange={() => changeLocale(this.value())}
-            menuItems={languages}
-            itemLabel="label"
-            itemValue="value"
-            className="md-cell"
-            position={SelectField.Positions.BELOW}
-            
-            />
-}
 
+    const handleLanguageChange = (value) => {
+        changeLocale(value);
+    }
+
+    return <div id="languages">
+                <i class="fas fa-globe-americas language-icon"></i>
+                <SelectField
+                    id="languageSelector"
+                    key="language"
+                    placeholder="Objects button"
+                    className="md-cell language-selector"
+                    menuItems={languages}
+                    onChange={handleLanguageChange}
+                    defaultValue={intl.locale}
+                    position={SelectField.Positions.BELOW}
+                />
+            </div>
+}
