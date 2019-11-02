89c652e1fba940e2aceb90fa189179a88b7eac70
DouglasFuelber <dffuelber@gmail.com>
2019-11-02 17:27:53 -0300

    Translate contact page and change contact form component structure

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,152 +1,145 @@
-import React, { Component } from "react";
-import ReCAPTCHA from "react-google-recaptcha";
-import Button from "react-md/lib/Buttons";
-import config from "../../../data/SiteConfig";
+import React, { useState } from "react";
 import { TextField } from 'react-md';
+import Button from "react-md/lib/Buttons";
+import { useIntl } from "gatsby-plugin-intl"
+import ReCAPTCHA from "react-google-recaptcha";
+
+import config from "../../../data/SiteConfig";
+
 import "./ContactForm.scss";
 
-class ContactForm extends Component {
-    constructor(props) {
-        super(props);
+export default () => {
 
-        this.state = {
-            fields: {},
-            errors: {}
-        }
+    const intl = useIntl();
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleValidation() {
-        let fields = this.state.fields;
+    const [fields, setFields] = useState({});
+    const [errors, setErrors] = useState({});
+
+    const handleValidation = () => {
+        let validationFields = fields;
         let errors = {};
         let formIsValid = true;
 
         //Name
-        if (!fields["Name"]) {
+        if (!validationFields["Name"]) {
             formIsValid = false;
-            errors["Name"] = "Por favor, me informe seu nome";
+            errors["Name"] = intl.formatMessage({ id: "components.contactForm.nameRequired" });
         }
         else {
             errors["Name"] = "";
         }
 
         //Email
-        if (!fields["Email"]) {
+        if (!validationFields["Email"]) {
             formIsValid = false;
-            errors["Email"] = "Por favor, me informe seu email";
+            errors["Email"] = intl.formatMessage({ id: "components.contactForm.emailRequired" });
         }
-        else if (typeof fields["Email"] !== "undefined") {
-            let lastAtPos = fields["Email"].lastIndexOf('@');
-            let lastDotPos = fields["Email"].lastIndexOf('.');
+        else if (typeof validationFields["Email"] !== "undefined") {
+            let lastAtPos = validationFields["Email"].lastIndexOf('@');
+            let lastDotPos = validationFields["Email"].lastIndexOf('.');
 
-            if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["Email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["Email"].length - lastDotPos) > 2)) {
+            if (!(lastAtPos < lastDotPos && lastAtPos > 0 && validationFields["Email"].indexOf('@@') == -1 && lastDotPos > 2 && (validationFields["Email"].length - lastDotPos) > 2)) {
                 formIsValid = false;
-                errors["Email"] = "Por favor, me informe um email válido";
+                errors["Email"] = intl.formatMessage({ id: "components.contactForm.emailInvalid" });
             }
         } else {
             errors["Email"] = "";
         }
 
         //Message
-        if (!fields["Message"]) {
+        if (!validationFields["Message"]) {
             formIsValid = false;
-            errors["Message"] = "Conte-me o motivo do seu contato, por favor";
+            errors["Message"] = intl.formatMessage({ id: "components.contactForm.messageRequired" });
         } else {
             errors["Message"] = "";
         }
 
         //Recaptcha
-        if (!fields["Recaptcha"]) {
+        if (!validationFields["Recaptcha"]) {
             formIsValid = false;
-            errors["Recaptcha"] = "Por favor, marque o reCaptcha acima";
+            errors["Recaptcha"] = intl.formatMessage({ id: "components.contactForm.recaptchaRequired" });
         } else {
             errors["Recaptcha"] = "";
-        }        
+        }
 
-        this.setState({ errors: errors });
+        setErrors(errors);
         return formIsValid;
     }
 
-    handleSubmit(e) {
+    const handleSubmit = (e) => {
         e.preventDefault();
 
-        if (this.handleValidation()) {
+        if (handleValidation()) {
             e.target.submit();
         }
     }
-    handleChange(field, e) {
-        let fields = this.state.fields;        
-        fields[field] = e;
-        this.setState({ fields });      
+
+    const handleChange = (field, e) => {
+        let validationFields = fields;
+        validationFields[field] = e;
+        setFields(validationFields);
     }
-    render() {
-        const recaptchaRef = React.createRef();
-        return (
-            <div className="contact-container mobile-fix">
-                <div className="contact-wrapper md-cell--center">
-                    <div className="md-cell--12">
-                        <p>Se quiser entrar em contato comigo, deixe-me uma mensagem:</p>
-                    </div>
-                    <div id="form">
-                        <form action="/success" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.handleSubmit}>
-                            <input type="hidden" name="form-name" value="contact" />
-                            <div className="md-grid">
 
-                                <TextField
-                                    id="name"
-                                    label="Nome"
-                                    name="Name"
-                                    ref="Name"
-                                    className="md-cell--12 md-cell--bottom"
-                                    onChange={this.handleChange.bind(this, "Name")}
-                                    value={this.state.fields["Name"]}
-                                    required />
-                                <div class="md-text-field-message-container md-full-width md-text--error">{this.state.errors["Name"]}</div>
+    return (
+        <div className="contact-container mobile-fix">
+            <div className="contact-wrapper md-cell--center">
+                <div className="md-cell--12">
+                    <p>{intl.formatMessage({ id: "components.contactForm.sentence" })}</p>
+                </div>
+                <div id="form">
+                    <form action={`/${intl.locale}/success/`} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleSubmit}>
+                        <input type="hidden" name="form-name" value="contact" />
+                        <div className="md-grid">
 
-                                <TextField
-                                    id="email"
-                                    label="Email"
-                                    name="Email"
-                                    className="md-cell--12 md-cell--bottom"
-                                    type="email"
-                                    onChange={this.handleChange.bind(this, "Email")}
-                                    value={this.state.fields["Email"]}
-                                    ref="Email"
-                                    required />
-                                <div class="md-text-field-message-container md-full-width md-text--error">{this.state.errors["Email"]}</div>
+                            <TextField
+                                id="name"
+                                label={intl.formatMessage({ id: "components.contactForm.nameLabel" })}
+                                name="Name"
+                                className="md-cell--12 md-cell--bottom"
+                                onChange={handleChange.bind(this, "Name")}
+                                value={fields["Name"]}
+                                required />
+                            <div class="md-text-field-message-container md-full-width md-text--error">{errors["Name"]}</div>
 
-                                <TextField
-                                    id="message"
-                                    label="Mensagem"
-                                    name="Message"
-                                    rows={5}
-                                    className="md-cell--12 md-cell--bottom"
-                                    onChange={this.handleChange.bind(this, "Message")}
-                                    value={this.state.fields["Message"]}
-                                    ref="Message"
-                                    required />
-                                <div class="md-text-field-message-container md-full-width md-text--error">{this.state.errors["Message"]}</div>
+                            <TextField
+                                id="email"
+                                label={intl.formatMessage({ id: "components.contactForm.emailLabel" })}
+                                name="Email"
+                                className="md-cell--12 md-cell--bottom"
+                                type="email"
+                                onChange={handleChange.bind(this, "Email")}
+                                value={fields["Email"]}
+                                required />
+                            <div class="md-text-field-message-container md-full-width md-text--error">{errors["Email"]}</div>
 
-                                <div className="md-cell--6">
-                                    <br />
-                                    <ReCAPTCHA
-                                        ref={recaptchaRef}
-                                        sitekey={config.recaptchaPublicKey}
-                                        onChange={this.handleChange.bind(this, "Recaptcha")} />
-                                    <div class="md-text-field-message-container md-full-width md-text--error">{this.state.errors["Recaptcha"]}</div>
-                                    
-                                </div>
-                                <div className="md-cell--6">
-                                    <br />
-                                    <Button flat className="submit-button secondary-button" type="submit">Enviar</Button>
-                                </div>
+                            <TextField
+                                id="message"
+                                label={intl.formatMessage({ id: "components.contactForm.messageLabel" })}
+                                name="Message"
+                                rows={5}
+                                className="md-cell--12 md-cell--bottom"
+                                onChange={handleChange.bind(this, "Message")}
+                                value={fields["Message"]}
+                                required />
+                            <div class="md-text-field-message-container md-full-width md-text--error">{errors["Message"]}</div>
+
+                            <div className="md-cell--6">
+                                <br />
+                                <ReCAPTCHA
+                                    sitekey={config.recaptchaPublicKey}
+                                    onChange={handleChange.bind(this, "Recaptcha")}
+                                    hl={intl.locale}/>
+                                <div class="md-text-field-message-container md-full-width md-text--error">{errors["Recaptcha"]}</div>
+
                             </div>
-                        </form>
-                    </div>
+                            <div className="md-cell--6">
+                                <br />
+                                <Button flat className="submit-button secondary-button" type="submit">{intl.formatMessage({ id: "components.contactForm.buttonLabel" })}</Button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default ContactForm;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import Helmet from "react-helmet";
-import Layout from "../components/Layout";
+import { useIntl } from "gatsby-plugin-intl";
+
 import ContactForm from "../components/ContactForm";
+import Layout from "../components/Layout";
+
 import config from "../../data/SiteConfig";
+
 import "./contact.scss";
 
-const ContactPage = ({ location }) =>
-  <Layout location={location} title="Contato">
+const ContactPage = ({ location }) => {
+  const intl = useIntl();
+
+  return <Layout location={location} title={intl.formatMessage({ id: "contact.pageTitle" })}>
 
     <Helmet>
-      <title>{`Contato | ${config.siteTitle}`}</title>
-      <link rel="canonical" href={`${config.siteUrl}/contact/`} />
+      <title>{`${intl.formatMessage({ id: "contact.pageTitle" })} | ${config.siteTitle}`}</title>
+      <link rel="canonical" href={`${config.siteUrl}/${intl.locale}/contact/`} />
     </Helmet>
 
     <div id="contact-container" className="tertiary_bg">
 
       <div id="page_title" className="md-grid md-cell--8">
-        <h1 className="left-border-area light-border">Contato</h1>
+        <h1 className="left-border-area light-border">{intl.formatMessage({ id: "contact.pageTitle" })}</h1>
       </div>
 
       <div id="page_content">
@@ -24,5 +30,6 @@ const ContactPage = ({ location }) =>
       </div>
     </div>
   </Layout>
+}
 
 export default ContactPage;
