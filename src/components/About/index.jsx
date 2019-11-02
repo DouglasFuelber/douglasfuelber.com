e31e05589ae5df4b13fc56b224d108c20f95fb5e
DouglasFuelber <dffuelber@gmail.com>
2019-11-02 18:10:50 -0300

    Translate about component

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,28 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
+import { useIntl } from "gatsby-plugin-intl";
+
 import config from "../../../data/SiteConfig";
+
 import "./About.scss";
 
-class About extends Component {
-  render() {
-    return (
-      <div className="about-container mobile-fix">
-        <div className="about-wrapper md-cell--center">
-          <div className="md-grid">
-            <div className="md-cell--center md-cell--4 md-text-center">
-              <img
-                src={config.userAvatar}
-                className="about-img"
-                alt={config.userName}
-              />
-            </div>
-            <div className="md-cell--center md-cell--middle md-cell--8">
-              <p className="about-text left-border-area light-border">{config.userDescription}</p>
-            </div>
-          </div>
+export default () => {
+  const intl = useIntl();
+  return <div className="about-container mobile-fix">
+    <div className="about-wrapper md-cell--center">
+      <div className="md-grid">
+        <div className="md-cell--center md-cell--4 md-text-center">
+          <img
+            src={config.userAvatar}
+            className="about-img"
+            alt={config.userName}
+          />
+        </div>
+        <div className="md-cell--center md-cell--middle md-cell--8">
+          <p className="about-text left-border-area light-border">{intl.formatMessage({ id: "site.userDescription" })}</p>
         </div>
       </div>
-    );
-  }
+    </div>
+  </div>
 }
-
-export default About;
