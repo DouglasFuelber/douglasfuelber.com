8bcfc2c4f1fdc91bf89b88b9a8f0eda17805c6b6
DouglasFuelber <dffuelber@gmail.com>
2019-11-02 17:51:43 -0300

    Translate About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
+import { useIntl } from "gatsby-plugin-intl";
+
 import Layout from "../components/Layout";
 import About from "../components/About";
 import LinkedInBadge from "../components/LinkedInBadge";
@@ -8,19 +10,23 @@ import ResumeEducation from "../components/ResumeEducation";
 import ResumeLanguages from "../components/ResumeLanguages";
 import ResumePublications from "../components/ResumePublications";
 import ResumeSkills from "../components/ResumeSkills";
+
 import config from "../../data/SiteConfig";
+
 import "./about.scss";
 
-const AboutPage = ({ location }) =>
-  <Layout location={location} title="Sobre mim">
+const AboutPage = ({ location }) => {
+  const intl = useIntl();
+
+  return <Layout location={location} title={intl.formatMessage({ id: "about.pageTitle" })}>
     <div id="about-container" className="tertiary_bg">
       <Helmet>
-        <title>{`Sobre mim | ${config.siteTitle}`}</title>
-        <link rel="canonical" href={`${config.siteUrl}/sobre/`} />
+        <title>{`${intl.formatMessage({ id: "about.pageTitle" })} | ${config.siteTitle}`}</title>
+        <link rel="canonical" href={`${config.siteUrl}/${intl.locale}/about/`} />
       </Helmet>
 
       <div id="page_title" className="md-grid md-cell--8">
-        <h1 className="left-border-area light-border">Sobre mim</h1>
+        <h1 className="left-border-area light-border">{intl.formatMessage({ id: "about.pageTitle" })}</h1>
       </div>
       <div id="page_content">
         <About />
@@ -33,5 +39,6 @@ const AboutPage = ({ location }) =>
       </div>
     </div>
   </Layout>
+}
 
 export default AboutPage;
