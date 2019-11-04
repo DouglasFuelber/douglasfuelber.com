62021bb18b69ef236b966dc8120560c238e8f9c1
DouglasFuelber <dffuelber@gmail.com>
2019-11-03 22:25:19 -0300

    Translate LinkedIn component

--- src/components/LinkedInBadge/index.jsx
+++ src/components/LinkedInBadge/index.jsx
@@ -1,32 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
+import { useIntl } from "gatsby-plugin-intl";
+
 import "./LinkedInBadge.scss";
 
-class LinkedInBadge extends Component {
-  render() {
+export default () => {
+  if (typeof document !== `undefined`) {
+    var aScript = document.createElement('script');
+    aScript.type = 'text/javascript';
+    aScript.src = "https://platform.linkedin.com/badges/js/profile.js";
+    aScript.async = true;
+    aScript.defer = true;
+    document.head.appendChild(aScript);
+  }
 
-    if (typeof document !== `undefined`) {
-      var aScript = document.createElement('script');
-      aScript.type = 'text/javascript';
-      aScript.src = "https://platform.linkedin.com/badges/js/profile.js";
-      aScript.async = true;
-      aScript.defer = true;
-      document.head.appendChild(aScript);
-    }  
+  const intl = useIntl();
+  const locale = intl.locale == 'en' ? 'en_US' : 'pt_BR';
 
-    return (
-      <div className="linkedin-badge-container mobile-fix">
-        <div className="linkedin-badge-wrapper md-cell--center">
-          <div className="md-cell--center md-cell--middle md-cell--12">
-            <div className="badge left-border-area light-border">
-              <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="pt_BR" data-type="horizontal" data-theme="light" data-vanity="douglasfuelber">
-                  <a class="LI-simple-link" href='https://br.linkedin.com/in/douglasfuelber/en?trk=profile-badge'>Douglas Fuelber</a>
-              </div>
-            </div>
+  return <div className="linkedin-badge-container mobile-fix">
+    <div className="linkedin-badge-wrapper md-cell--center">
+      <div className="md-cell--center md-cell--middle md-cell--12">
+        <div className="badge left-border-area light-border">
+          <div class="LI-profile-badge" data-version="v1" data-size="medium" data-locale={locale} data-type="horizontal" data-theme="light" data-vanity="douglasfuelber">
+            <a class="LI-simple-link" href={`https://br.linkedin.com/in/douglasfuelber/${intl.locale}?trk=profile-badge`}>Douglas Fuelber</a>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  </div>
 }
-
-export default LinkedInBadge;
