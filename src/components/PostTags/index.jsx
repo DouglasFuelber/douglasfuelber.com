1b3722a5e253a0f413000f6f2d78f5a9b79e07eb
DouglasFuelber <dffuelber@gmail.com>
2019-11-28 12:17:38 -0300

    Change post tags from class to function

--- src/components/PostTags/index.jsx
+++ src/components/PostTags/index.jsx
@@ -1,27 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import _ from "lodash";
 import { Link } from "gatsby-plugin-intl";
 import Chip from "react-md/lib/Chips";
 import "./PostTags.scss";
 
-class PostTags extends Component {
-  render() {
-    const { tags } = this.props;
-    return (
-      <div className="post-tag-container">
-        {tags &&
-          tags.map(tag => (
-            <Link
-              key={tag}
-              style={{ textDecoration: "none" }}
-              to={`/blog/tags/${_.kebabCase(tag)}`}
-            >
-              <Chip label={tag} className="post-preview-tags" />
-            </Link>
-          ))}
-      </div>
-    );
-  }
+export default ({ tags }) => {
+  return (
+    <div className="post-tag-container">
+      {tags &&
+        tags.map(tag => (
+          <Link
+            key={tag}
+            style={{ textDecoration: "none" }}
+            to={`/blog/tags/${_.kebabCase(tag)}`}
+          >
+            <Chip label={tag} className="post-preview-tags" />
+          </Link>
+        ))}
+    </div>
+  );
 }
-
-export default PostTags;
