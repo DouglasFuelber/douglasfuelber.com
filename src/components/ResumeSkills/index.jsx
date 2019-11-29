781f7fea06a11d9eaa3fbe52244b9627b36cabe0
DouglasFuelber <dffuelber@gmail.com>
2019-11-29 17:32:50 -0300

    Adjust in skills display

--- src/components/ResumeSkills/index.jsx
+++ src/components/ResumeSkills/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, Cell } from 'react-md';
 import { useIntl } from "gatsby-plugin-intl";
 
 import { resume }  from "../../i18n/en";
@@ -10,9 +11,9 @@ export default () => {
 
     const getSkills = () => {
         return Object.keys(resume.skills).map(key => (
-            <div className="skill left-border-area light-border">
+            <Cell size="4" className="skill left-border-area light-border">
                 <h4 className="skill-title">{intl.formatMessage({ id: `resume.skills.${key}` })}</h4>
-            </div>
+            </Cell>
         ));
     }
 
@@ -20,9 +21,9 @@ export default () => {
         <div className="skills-wrapper md-cell--center">
             <div className="md-cell--center md-cell--middle md-cell--12">
                 <h3>{intl.formatMessage({ id: `components.resumeSkills.title` })}</h3>
-                <div id="skills">
+                <Grid id="skills">
                     {getSkills()}
-                </div>
+                </Grid>
             </div>
         </div>
     </div>
