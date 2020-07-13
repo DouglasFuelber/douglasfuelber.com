2ff2b52aebb9260b81092914f64ff00e17f411e8
Douglas Fuelber <contato@douglasfuelber.com>
2020-07-12 21:48:33 -0300

    Removed unused packages

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import Helmet from "react-helmet";
-import { useIntl } from "gatsby-plugin-intl"
-import Navigation from "../Navigation";
+import React from 'react';
+import Helmet from 'react-helmet';
+import { useIntl } from 'gatsby-plugin-intl';
+import Navigation from '../Navigation';
 
-import config from "../../data/site-data";
+import config from '../../data/site-data';
 
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "@fortawesome/fontawesome-free/js/all.min.js";
-
-import "./index.scss";
-import "../../layout/global.scss";
+import './index.scss';
+import '../../layout/global.scss';
 
 export default ({ children }) => {
-  const intl = useIntl()
+  const intl = useIntl();
   return (
     <Navigation config={config}>
       <div>
         <Helmet>
           <html lang={intl.locale} />
-          <meta name="description" content={intl.formatMessage({ id: "site.description" })} />
+          <meta
+            name="description"
+            content={intl.formatMessage({ id: 'site.description' })}
+          />
         </Helmet>
         {children}
       </div>
     </Navigation>
-  )
-}
+  );
+};
