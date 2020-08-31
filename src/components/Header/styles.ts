7c82ac38343d1379eb3ffe025411f377b63abcee
Douglas Fuelber <contato@douglasfuelber.com>
2020-08-30 21:11:06 -0300

    Menu layout

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,12 +2,57 @@ import styled from 'styled-components';
 
 export const Container = styled.header`
   background: #00143C;
-  border-bottom: 3px solid #70A1FF;
-  height: 80px;
+  border-bottom: 4px solid #70A1FF;
+  display: flex;
+  height: 72px;
+  justify-content: center;
   width: 100%;
 `;
 
 export const ContentWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex: 1;
+  flex-direction: row;
   height: 100%;
-  max-width: 1140px;
+  justify-content: space-between;
+  max-width: 1144px;
+`;
+
+export const Nav = styled.nav`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+
+  img {
+    height: 24px;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+    margin-left: 24px;
+
+    li {
+      a {
+        border-radius: 24px;
+        color: #eee;
+        font-size: 14px;
+        font-weight: 700;
+        margin-right: 4px;
+        text-transform: uppercase;
+        padding: 4px 12px;
+
+        &:hover {
+          background-color: #001950;
+        }
+
+        &.active {
+          background-color: #eee;
+          color: #00143C;
+        }
+      }
+    }
+  }
 `;
