136fc3433ac97eab5d6c1adec44cb4b60bbc532c
Douglas Fuelber <contato@douglasfuelber.com>
2020-09-07 18:06:11 -0300

    Contact success page layout

---
+++ src/pages/success-styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    color: #70a1ff;
+    font-size: 56px;
+    font-weight: 600;
+    line-height: 56px;
+  }
+
+  h2 {
+    color: #00143c;
+    font-weight: 800;
+    margin-bottom: 16px;
+    margin-top: 16px;
+  }
+`;
+
+export const LinkButton = styled.a`
+  background-color: #00143c;
+  border: none;
+  border-radius: 8px;
+  color: #fff;
+  font-weight: 600;
+  padding: 8px 16px;
+
+  &:hover {
+    background: ${darken(0.05, '#00143c')};
+  }
+`;
---
+++ src/pages/success.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useIntl } from 'gatsby-plugin-intl';
+
+import Layout from '../components/Layout';
+
+import { Container, LinkButton } from './success-styles';
+
+const SuccessContact: React.FC = () => {
+  const intl = useIntl();
+
+  return (
+    <Layout
+      pageTitle={intl.formatMessage({ id: 'success.pageTitle' })}
+      pageRelativeUrl="success"
+    >
+      <Container>
+        <h1>{`${intl.formatMessage({ id: 'success.pageTitle' })}!`}</h1>
+        <h2>{intl.formatMessage({ id: 'success.subtitle' })}</h2>
+        <LinkButton href={`/${intl.locale}/`}>
+          {intl.formatMessage({ id: 'success.button' })}
+        </LinkButton>
+      </Container>
+    </Layout>
+  );
+};
+
+export default SuccessContact;
