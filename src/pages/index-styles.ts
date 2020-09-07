6ccaffaededf04d56b261a2105d160ca014c6d98
Douglas Fuelber <contato@douglasfuelber.com>
2020-09-07 01:31:39 -0300

    Finished home page

--- src/pages/index-styles.ts
+++ src/pages/index-styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'gatsby-plugin-intl';
 
 export const Container = styled.div`
   display: flex;
@@ -10,8 +11,12 @@ export const Container = styled.div`
 `;
 
 export const TitleWrapper = styled.div`
-  border-left: 4px solid #00143C;
+  border-left: 2px solid #00143C;
   padding-left: 16px;
+
+  @media(min-width: 768px) {
+    border-left-width: 4px;
+    }
 
   h1 {
     color: #70A1FF;
@@ -21,16 +26,18 @@ export const TitleWrapper = styled.div`
 
     @media(min-width: 768px) {
       font-size: 40px;
-      line-height: 56px;
+      line-height: 48px;
     }
 
     strong {
       color: #00143C;
-      font-size: 36px;
+      font-size: 40px;
       font-weight: 800;
+      line-height: 48px;
 
       @media(min-width: 768px) {
-        font-size: 56px;
+        font-size: 64px;
+        line-height: 72px;
       }
     }
   }
@@ -39,13 +46,76 @@ export const TitleWrapper = styled.div`
     color: #70A1FF;
     font-size: 24px;
     font-weight: 800;
+    line-height: 32px;
 
     @media(min-width: 768px) {
       font-size: 40px;
+      line-height: 48px;
     }
   }
 `;
 
 export const IconsWrapper = styled.div`
+  align-items: start;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin-top: 64px;
 
+  @media(min-width: 768px) {
+    margin-top: 112px;
+  }
 `;
+
+export const ItemLink = styled(Link)`
+  align-items: center;
+  display: flex;
+  margin-bottom: 24px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  svg {
+    color: #001950;
+    height: 32px;
+    width: 32px;
+
+    @media(min-width: 768px) {
+      height: 48px;
+      width: 48px;
+    }
+  }
+
+  .info {
+    display: flex;
+    flex-direction: column;
+    margin-left: 8px;
+
+    @media(min-width: 768px) {
+      margin-left: 16px;
+    }
+
+    span {
+      color: #70A1FF;
+      font-size: 12px;
+      font-weight: 600;
+
+      @media(min-width: 768px) {
+        font-size: 20px;
+      }
+    }
+
+    h3 {
+      color: #001950;
+      font-weight: 800;
+      font-size: 21px;
+      letter-spacing: -0.015em;
+      text-transform: uppercase;
+
+      @media(min-width: 768px) {
+        font-size: 32px;
+      }
+    }
+  }
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import { useIntl } from 'gatsby-plugin-intl';
+import { FaUser, FaBookOpen, FaEnvelope } from 'react-icons/fa';
 
 import Layout from '../components/Layout';
 
 import config from '../data/site-data';
 
-import { Container, TitleWrapper, IconsWrapper } from './index-styles';
+import { Container, TitleWrapper, IconsWrapper, ItemLink } from './index-styles';
 
 export default () => {
   const intl = useIntl();
@@ -18,11 +19,37 @@ export default () => {
       </Helmet>
 
       <Container>
+
         <TitleWrapper>
-          <h1>Hello,<br /> I am <strong>Douglas Fuelber</strong></h1>
-          <h2>Full Stack Web Developer</h2>
+          <h1>{intl.formatMessage({ id: "home.hello" })},<br /> {intl.formatMessage({ id: "home.iam" })} <strong>Douglas Fuelber</strong></h1>
+          <h2>{intl.formatMessage({ id: "home.subtitle" })}</h2>
         </TitleWrapper>
+
         <IconsWrapper>
+
+          <ItemLink to="/about/">
+            <FaUser />
+            <div className="info">
+              <span>{intl.formatMessage({ id: "navigation.about.subtitle" })}</span>
+              <h3>{intl.formatMessage({ id: "navigation.about.title" })}</h3>
+            </div>
+          </ItemLink>
+
+          <ItemLink to="/blog/">
+            <FaBookOpen />
+            <div className="info">
+              <span>{intl.formatMessage({ id: "navigation.blog.subtitle" })}</span>
+              <h3>{intl.formatMessage({ id: "navigation.blog.title" })}</h3>
+            </div>
+          </ItemLink>
+
+          <ItemLink to="/contact/">
+            <FaEnvelope />
+            <div className="info">
+              <span>{intl.formatMessage({ id: "navigation.contact.subtitle" })}</span>
+              <h3>{intl.formatMessage({ id: "navigation.contact.title" })}</h3>
+            </div>
+          </ItemLink>
 
         </IconsWrapper>
       </Container>
